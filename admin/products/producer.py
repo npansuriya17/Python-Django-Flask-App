7f62ad59c6df0657234a52ff7883e0c50c08ccc2
npansuriya17 <nidhipansuriya17@gmail.com>
2021-05-11 16:20:59 -0500

    first commit

---
+++ admin/products/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://dhjfkjnk:27zj5yp9OGMVMYzWzHS-qrCvSLxABvBv@baboon.rmq.cloudamqp.com/dhjfkjnk')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
